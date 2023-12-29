c27a995f9186d6ffc5ac1f0825c80d5016187f97
Arian <arian@MacBook-Pro-de-Arian.local>
2023-12-29 23:19:23 +0100

    Fix - Scroll down position on mobile display

--- src/components/IconScroll.jsx
+++ src/components/IconScroll.jsx
@@ -1,7 +1,7 @@
 import { RxArrowDown } from 'react-icons/rx';
 export const IconScroll = () => {
 	return (
-		<div className="w-auto absolute bottom-0 right-0 h-64 pr-2 flex flex-col justify-end items-stretch text-center gap-4 font-Montserrat md:w-full md:justify-center md:items-center text-purple-400 font-semibold">
+		<div className="w-auto absolute bottom-0 right-0 h-64 mb-24 pr-2 flex flex-col justify-end items-stretch text-center gap-4 font-Montserrat md:w-full md:justify-center md:items-center text-purple-400 font-semibold">
 			<p className="p-2 ">
 				<span className="text-md [writing-mode:vertical-lr] md:[writing-mode:horizontal-tb] md:text-2xl">
 					Scroll Down
